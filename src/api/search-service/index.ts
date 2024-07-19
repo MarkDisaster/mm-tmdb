dc8b3aee3014096939db9817e2b16fdb7a64519d
Marek Marek <marekmarek@Marek-MacBook-Pro.local>
2024-07-19 21:30:05 +0200

    Added getMovies method to API services

--- src/api/search-service/index.ts
+++ src/api/search-service/index.ts
@@ -3,7 +3,7 @@ import api from "../api";
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const getMovies = async (params: Record<any, any>) => {
 	const res = await api.get(
-		`order-api/orders`,
+		`search/movie`,
 		params,
 	);
 
