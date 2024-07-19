b4814b8d29ef1fa697f273898083a7d1747d3dcf
Marek Marek <marekmarek@Marek-MacBook-Pro.local>
2024-07-19 22:36:27 +0200

    cleaning

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,4 @@
 import MoviesComparingPage from './pages/MoviesComparing';
-
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 
@@ -9,17 +6,7 @@ const App = () => {
 
   return (
     <>
-    <MoviesComparingPage />
-
-    <div>
-      <a href="https://vitejs.dev" target="_blank">
-        <img src={viteLogo} className="logo" alt="Vite logo" />
-      </a>
-      <a href="https://react.dev" target="_blank">
-        <img src={reactLogo} className="logo react" alt="React logo" />
-      </a>
-    </div>
-    <h1>Vite + React + TypeScript + SWC</h1>
+      <MoviesComparingPage />
     </>
   )
 }
