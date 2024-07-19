c0e54e2fe64d60b3f7b4e48a2294c822a01494c6
Marek Marek <marekmarek@Marek-MacBook-Pro.local>
2024-07-19 17:09:31 +0200

    Start the Project. Tech: React, TypeScript, SWC, Vite, React Core UI. Set up auto import for components.

---
+++ src/App.tsx
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+
+import { CAlert, CContainer } from '@coreui/react';
+
+
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <CAlert color="primary">
+      A simple primary alert—check it out!a
+    </CAlert>
+    <CContainer sm>100% wide until small breakpoint</CContainer>
+
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React + TypeScript + SWC</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
