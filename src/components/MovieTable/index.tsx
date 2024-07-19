fedcd6ee8803199c9d7c9dcbd7da1a59b66f9085
Marek Marek <marekmarek@Marek-MacBook-Pro.local>
2024-07-19 22:36:59 +0200

    MovieTable Component checkbox...

--- src/components/MovieTable/index.tsx
+++ src/components/MovieTable/index.tsx
@@ -1,4 +1,7 @@
 import { CImage, CTable, CTableBody, CTableDataCell, CTableRow } from '@coreui/react';
+import { CIcon } from '@coreui/icons-react';
+import { cilImage } from '@coreui/icons';
+
 import { MovieTablesProps } from './interfaces';
 import { TMDB_IMG_URI } from '../../api/constants';
 
@@ -10,13 +13,19 @@ if (isLoading) return (<div>Loading...</div>)
     <CTable color="light" striped hover>
     <CTableBody>
         <CTableRow>
-          <CTableDataCell className='fw-bold'>Movie Name</CTableDataCell>
+          <CTableDataCell className='fw-bold'></CTableDataCell>
+          <CTableDataCell className='fw-bold'>Poster</CTableDataCell>
+          <CTableDataCell className='fw-bold'>Name</CTableDataCell>
         </CTableRow>
           {movies?.map((movie, index) => {
             return (
-              <CTableRow key={index}>
-                <CTableDataCell>
-                  <CImage align="start" rounded src={`${TMDB_IMG_URI}${movie.poster_path}`} height={125} />
+              <CTableRow key={index} className='mb-2'>
+                <CTableDataCell>checkbox</CTableDataCell>
+                <CTableDataCell className='p-0'>
+                  {movie.poster_path
+                    ? <CImage align="start" src={`${TMDB_IMG_URI}${movie.poster_path}`} width={50} />
+                    : <CIcon icon={cilImage} height={50} className='text-secondary' />
+                  }
                 </CTableDataCell>
                 <CTableDataCell>{movie.original_title}</CTableDataCell>
               </CTableRow>)
