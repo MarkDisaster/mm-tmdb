e52225bb5ef85cd61ebdcd68e2ad8cb5fb8632f0
Marek Marek <marekmarek@Marek-MacBook-Pro.local>
2024-07-19 21:11:00 +0200

    Added queryClient and QueryClientProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
 import './index.css'
-
 import '@coreui/coreui/dist/css/coreui.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			retry: 0,
+			staleTime: 10000,
+			refetchOnMount: false,
+			refetchOnWindowFocus: false,
+		},
+	},
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+    
   </React.StrictMode>,
 )
