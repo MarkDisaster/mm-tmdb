9ae06bdbc9dcedcf9687872615c7d36e78845133
Marek Marek <marekmarek@Marek-MacBook-Pro.local>
2024-07-22 15:16:41 +0200

    Fixed getMoviesBarChartValues

--- src/helpers/getMoviesBarChartValues/getMoviesBarChartValues.ts
+++ src/helpers/getMoviesBarChartValues/getMoviesBarChartValues.ts
@@ -2,10 +2,6 @@ import { MovieBarChartProps } from './interfaces';
 
 export const getMoviesBarChartValues = (movies: MovieBarChartProps[]) => {
    return movies.map((movie) => {
-      const numberWithDot = movie.popularity;
-      const numberString = numberWithDot.toString();
-      const cleanedString = numberString.replace('.', '');
-      const finalNumber = Number(cleanedString);
-      return { name: movie.original_title, popularity: finalNumber };
+      return { name: movie.original_title, popularity: movie.popularity };
    });
 };
