2775fcdabd745b78cfca2e64f7808129ff7bcdde
Marek Marek <marekmarek@Marek-MacBook-Pro.local>
2024-08-05 15:53:00 +0200

    youtube video height

--- src/components/YoutubeVideo/index.tsx
+++ src/components/YoutubeVideo/index.tsx
@@ -6,7 +6,7 @@ const YoutubeVideo = ({ youtubeUrl }: YoutubeVideoProps) => {
    return (
       <iframe
          width="100%"
-         height="250px"
+         height="200px"
          src={youtubeUrl}
          title="YouTube video player"
          frameBorder="0"
