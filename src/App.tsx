7f181033607174b15e65abe5f71cdc1a020b0167
Marek Marek <marekmarek@Marek-MacBook-Pro.local>
2024-08-05 12:50:29 +0200

    Edit MoviePage

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,36 @@
 import { Provider } from 'react-redux';
 import { rootStore } from './store/store';
 
-import Main from './components/Main';
-
-import './App.css';
 import RoutingService from './services/routing-service';
 import AuthorizationService from './services/authorization-service';
+
+import Main from './components/Main';
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+   apiKey: 'AIzaSyBAckoDteHOnYQuSKsz2BAZqd8vtwTu2_M',
+   authDomain: 'tmdbmm.firebaseapp.com',
+   projectId: 'tmdbmm',
+   storageBucket: 'tmdbmm.appspot.com',
+   messagingSenderId: '191709921532',
+   appId: '1:191709921532:web:93dcfc57d87cd8c3bd9779',
+   measurementId: 'G-VMKNS50PEC',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export { app, analytics };
+
+import './App.css';
 
 const App = () => {
    return (
